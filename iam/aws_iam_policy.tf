cbb95682513be2e8fc28d4a0b5dd126498f09a5b
Yuya Furusawa <fullyou0798@gmail.com>
2021-10-31 20:40:41 +0900

    Add IAM policy for ECS deploy

--- iam/aws_iam_policy.tf
+++ iam/aws_iam_policy.tf
@@ -5,6 +5,13 @@ resource "aws_iam_policy" "deploy-policy" {
   policy = file("aws_iam_policies/deploy_policy.json")
 }
 
+resource "aws_iam_policy" "deploy-ecs" {
+  name = "deploy-ecs"
+  path = "/"
+  description = "deploy ecs policy"
+  policy = file("aws_iam_policies/deploy.json")
+}
+
 resource "aws_iam_policy" "sample_ecs_instance_policy" {
   name = "sample-ecs-instance-policy"
   path = "/"
--- iam/aws_iam_user_policy_attachment.tf
+++ iam/aws_iam_user_policy_attachment.tf
@@ -2,3 +2,8 @@ resource "aws_iam_user_policy_attachment" "deploy-attach" {
   user = aws_iam_user.deploy-user.name
   policy_arn = aws_iam_policy.deploy-policy.arn
 }
+
+resource "aws_iam_user_policy_attachment" "deploy-ecs-attach" {
+  user = aws_iam_user.deploy-user.name
+  policy_arn = aws_iam_policy.deploy-ecs.arn
+}
